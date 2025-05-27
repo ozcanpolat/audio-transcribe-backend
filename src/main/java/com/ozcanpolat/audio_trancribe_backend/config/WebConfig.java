71c47feeb0ebcbd8ed044052cf584791410869fb
ozcanpolat <ozcanp132@gmail.com>
2025-05-27 13:17:42 +0300

    CQRS hatası çözüldü

--- src/main/java/com/ozcanpolat/audio_trancribe_backend/config/WebConfig.java
+++ src/main/java/com/ozcanpolat/audio_trancribe_backend/config/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "https://localhost:5173")
+                .allowedOrigins("https://audio-transcribe-frontend-rho.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
